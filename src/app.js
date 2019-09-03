3d8bd1dd00545a6443fb6c36c760f6267805fa78
Pheorix <jrix21@gmail.com>
2019-09-02 21:26:29 -0500

    Some setup

---
+++ src/app.js
@@ -0,0 +1,22 @@
+// Things I need
+
+import React from "react";
+//import components
+//render function to display components
+
+//character cards need a way to be shuffled 
+    //(for loop)
+
+//Game needs to keep track of points
+    // When user clicks, save value of card
+    // If previous value is the same as current value,
+        //lose game
+    //else
+        //keep playing.
+
+//Set High Score Variable.
+    //Compare user score to high score.
+    //If user score becomes higher than "High Score"
+        //update high score
+
+//export the app
